eee2cdac2feab0182c66bead4b2d54cb6b7e1586
shelly-meir <meirshelly@gmail.com>
2025-10-26 12:35:19 +0200

    add environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  spotify: {
+    clientId: 'YOUR_CLIENT_ID_HERE',
+    clientSecret: 'YOUR_CLIENT_SECRET_HERE',
+    redirectUri: 'https://localhost:4200/callback'
+  },
+};
